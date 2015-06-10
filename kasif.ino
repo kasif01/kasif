b48320b2948673232eac691dbba6dcb396c67b3d
Abdulkadir Yaman <abdulkadiryaman@gmail.com>
2015-06-10 12:43:36 +0300

    adding RF controlling, and command receive

--- kasif.ino
+++ kasif.ino
@@ -1,4 +1,6 @@
 #include <Servo.h>
+#include <SPI.h>
+#include "RF24.h"
 
 Servo claw;
 Servo wrist;
@@ -20,9 +22,28 @@ int wristPin = 11;
 int elbowPin = 10;
 int rimPin = 9;
 
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(7, 8);
+/**********************************************************/
 
+byte addresses[][6] = {"1Node", "2Node"};
+ 
 void setup() {
   Serial.begin(9600);
+  
+  // starting radio..........
+  radio.begin();
+
+  // Set the PA Level low to prevent power supply related issues since this is a
+  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW); 
+
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1, addresses[1]);
+ 
+  radio.startListening();
+  // starting radio ends....
+  
   pinMode(dir1PinA, OUTPUT);
   pinMode(dir2PinA, OUTPUT);
   pinMode(dir1PinB, OUTPUT);
@@ -45,24 +66,26 @@ int maxDegree = 60;
 
 void loop()
 {
+  
+  waitForCommand();
 
  // ************************************************* forward ***********//
 
  
   
-  
-  delay(500);
-  for (int i = 0; i < maxDegree; i++) {
-    delay(10);
-    servoForward(claw);
-  }
-
-
-   delay(500);
-  for (int i = 0; i < maxDegree; i++) {
-    delay(10);
-    servoBackward(claw);
-  }
+//  
+//  delay(500);
+//  for (int i = 0; i < maxDegree; i++) {
+//    delay(10);
+//    servoForward(claw);
+//  }
+//
+//
+//   delay(500);
+//  for (int i = 0; i < maxDegree; i++) {
+//    delay(10);
+//    servoBackward(claw);
+//  }
 
 //  delay(500);
 //  for (int i = 0; i < maxDegree; i++) {
@@ -179,4 +202,24 @@ void servoBackward(Servo servo) {
 
   if (position != 0)
     servo.write(position - 1);
+}
+
+
+void waitForCommand(){
+  
+  int command;
+
+  if ( radio.available()) {
+    // Variable for the received timestamp
+    while (radio.available()) {                                   // While there is data ready
+      radio.read( &command, sizeof(int) );             // Get the payload
+    }
+
+    radio.stopListening();                                        // First, stop listening so we can talk
+    radio.write( &command, sizeof(int) );              // Send the final one back.
+    radio.startListening();                                       // Now, resume listening so we catch the next packets.
+    Serial.print(F("Sent ack "));
+    Serial.println(command);
+  }
+  
 }
